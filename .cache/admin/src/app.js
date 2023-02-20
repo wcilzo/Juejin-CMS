1b1c45ea137ca445f64df8f470f80b7e52f10def
Wcilzo <911402070@qq.com>
2023-02-21 02:00:19 +0800

    更换sqlite3,更新数据库

---
+++ .cache/admin/src/app.js
@@ -0,0 +1,15 @@
+export default {
+  config: {
+    locales: ['zh-Hans'],
+    translations: {
+      'zh-Hans': {
+        'Auth.form.email.label': '9527@juejin.com',
+        Users: '9527@juejin.com',
+        City: 'Chinese (Simplified) (zh-Hans)',
+        // 这里是刚刚在设置中新建的国际化语言版本的ID
+        Id: '2',
+      },
+    },
+  },
+  bootstrap() {},
+}
